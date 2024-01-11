be4c298c307e5cc0bb8b8ac871229f7ca9c2928e
Do Nhat <dmnhat.pt@gmail.com>
2024-01-11 16:34:33 +0700

    Displaying images and using multiple constructor functions

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,11 +1,15 @@
-import 'package:basics/styled_text.dart';
 import 'package:flutter/material.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key, required this.color1, required this.color2});
+  const GradientContainer(
+      {super.key, required this.color1, required this.color2});
+
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
 
   final Color color1;
   final Color color2;
@@ -20,8 +24,11 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: StyledText("Hello World!"),
+      child: Center(
+        child: Image.asset(
+          'assets/images/dice-2.png',
+          width: 200,
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(
-          color1: Color.fromARGB(255, 26, 2, 80),
-          color2: Color.fromARGB(255, 45, 7, 98),
-        ),
+        body: GradientContainer.purple()
       ),
     ),
   );
